ceb66d3f9b3502fee58652befd8a34fb43d344f8
Piotr Stęclik <piotr@steclik.com>
2018-08-03 09:02:39 +0200

    IMetadataContainer extension method

--- app/AutoUml/_interfaces/IMetadataContainer.cs
+++ app/AutoUml/_interfaces/IMetadataContainer.cs
@@ -8,4 +8,21 @@ namespace AutoUml
         [NotNull]
         Dictionary<string, object> Metadata { get; }
     }
+
+    public static class MetadataContainerExtensions
+    {
+        public static string TryGetStringMetadata(this IMetadataContainer container, string key)
+        {
+            if (container == null)
+                return null;
+            if (!container.Metadata.TryGetValue(key, out var x)) return null;
+            switch (x)
+            {
+                case string s: return s;
+                case null: return null;
+            }
+
+            return x.ToString();
+        }
+    }
 }
